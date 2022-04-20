b0965e6bdf0cd20127ef29d76b453446b1b3fc50
Zema128 <vladzemec@icloud.com>
2022-04-20 15:57:15 +0300

    add TG bot

---
+++ src/main/java/telega/coffe/bot/bots/TestCoffeeZemaBot.java
@@ -0,0 +1,49 @@
+package telega.coffe.bot.bots;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Component
+public class TestCoffeeZemaBot extends TelegramLongPollingBot {
+
+
+    private String userName = "test_coffe_zema_bot";
+
+    private String token = "5350608805:AAFRDxUamfPv72NUTHRRNV-iX1hYmZoIHIU";
+
+
+    @Override
+    public String getBotUsername() {
+        return userName;
+    }
+
+    @Override
+    public String getBotToken() {
+        return token;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String message = update.getMessage().getText().trim();
+            String chatId = update.getMessage().getChatId().toString();
+            sendMsg(chatId, message);
+        }
+    }
+
+    public synchronized void sendMsg(String chatId, String msg){
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.enableMarkdown(true);
+        sendMessage.setChatId(chatId);
+        sendMessage.setText("Go naxer!");
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
